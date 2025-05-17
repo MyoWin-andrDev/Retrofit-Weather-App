45ba163037fc25336e91f14d3d53beefc32565c3
Phyo Hein ( Ezekiel ) <mwrcps11@gmail.com>
2025-05-17 23:12:49 +0700

    Check credentials

--- app/src/main/java/com/learning/retrofitweatherapp/util/constant.kt
+++ app/src/main/java/com/learning/retrofitweatherapp/util/constant.kt
@@ -1,4 +1,7 @@
 package com.learning.retrofitweatherapp.util
 
-const val BASE_URL = "http://api.weatherapi.com/v1/"
-const val APIKEY = "Enter WeatherAPI Here"
+const val BASE_URL = "https://api.weatherapi.com/v1/"
+const val APIKEY = "Enter Your API Here "
+const val SEARCH_END_POINT = "search.json"
+const val CURRENT_END_POINT = "current.json"
+const val ASTRONOMY_END_POINT = "astronomy.json"
